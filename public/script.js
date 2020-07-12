45ea0e270eb41e69e7c197dba4362e95e4f4b2bc
Hamza <hamzayousfi2@gmail.com>
2020-07-12 19:53:38 +0300

    api call successful. working on data prentation

--- public/script.js
+++ public/script.js
@@ -1,12 +1,10 @@
 const searchElement = document.querySelector("[data-city-search]");
 const searchBox = new google.maps.places.SearchBox(searchElement);
-console.log("lol");
 searchBox.addListener("places_changed", () => {
   const place = searchBox.getPlaces()[0];
   if (place == null) return;
   const latitude = place.geometry.location.lat();
   const longitude = place.geometry.location.lng();
-
   fetch("/weather", {
     method: "POST",
     headers: {
@@ -16,10 +14,11 @@ searchBox.addListener("places_changed", () => {
     body: JSON.stringify({
       latitude: latitude,
       longitude: longitude,
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-      }),
-  });
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      //setWeatherData(data, place.formatted_address);
+    });
 });
--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const express = require("express");
 const app = express();
 
 const axios = require("axios");
-const http = require("http");
 
 const cors = require("cors");
 app.use(cors());
@@ -17,17 +16,18 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-const server = http.createServer(app);
-
 app.post("/weather", (req, res) => {
-  console.log(req.body);
-  const url = `https://api.weatherstack.com/current
-  ?access_key=${API_KEY}
-  &query= ${req.body.latitude},${req.body.longitude}&units = m`;
-  axios({
-    url: url,
-    responseType: "json",
-  }).then((data) => res.json(data.data));
+  const params = {
+    access_key: API_KEY,
+    query: `${req.body.latitude},${req.body.longitude}`,
+  };
+  axios
+    .get("http://api.weatherstack.com/current", { params })
+
+    .then((data) => res.json(data.data.current))
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
-server.listen(PORT, console.log(`The Server Started on Port :  ${PORT} ...`));
+app.listen(PORT, console.log(`The Server Started on Port :  ${PORT} ...`));
